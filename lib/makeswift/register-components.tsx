369b39df0f43dd9b32a48652314898718c28f2f4
Andrew Reifman <andrewr23@gmail.com>
2023-05-07 22:38:15 -0400

    feat: navigation component

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -15,6 +15,7 @@ import {
   RichText,
 } from "@makeswift/runtime/controls";
 import { Marquee } from "../../components/Marquee";
+import { Navigation } from "../../components/Navigation";
 
 // Register your components here!
 
@@ -45,5 +46,74 @@ ReactRuntime.registerComponent(Marquee, {
     gap: Number({ label: "Gap", defaultValue: 48 }),
     fadeColor: Color({ label: "Fade color", defaultValue: "#ffffff" }),
     className: Style(),
+  },
+});
+
+ReactRuntime.registerComponent(Navigation, {
+  type: "navigation",
+  label: "Navigation",
+  props: {
+    className: Style(),
+    links: List({
+      label: "Main navigation",
+      type: Shape({
+        type: {
+          text: TextInput({
+            label: "Text",
+            defaultValue: "Link",
+            selectAll: true,
+          }),
+          subnavGroups: List({
+            label: "Subnav groups",
+            type: Shape({
+              type: {
+                heading: TextInput({
+                  label: "Heading",
+                  defaultValue: "Heading",
+                  selectAll: true,
+                }),
+                subnavLinks: List({
+                  label: "Links",
+                  type: Shape({
+                    type: {
+                      link: Link({ label: "On click" }),
+                      icon: Image({
+                        label: "Icon",
+                        format: Image.Format.WithDimensions,
+                      }),
+                      linkText: TextInput({
+                        label: "Link text",
+                        defaultValue: "Link text",
+                        selectAll: true,
+                      }),
+                      subtext: TextInput({
+                        label: "Subtext",
+                        defaultValue: "This is some subtext",
+                        selectAll: true,
+                      }),
+                    },
+                  }),
+                  getItemLabel(subnavLink) {
+                    return subnavLink?.linkText || "Link";
+                  },
+                }),
+              },
+            }),
+            getItemLabel(subnavGroup) {
+              return subnavGroup?.heading || "Heading";
+            },
+          }),
+        },
+      }),
+      getItemLabel(mainNavLink) {
+        return mainNavLink?.text || "Link";
+      },
+    }),
+    ctaText: TextInput({
+      label: "CTA text",
+      defaultValue: "Request a demo",
+      selectAll: true,
+    }),
+    ctaLink: Link({ label: "CTA on click" }),
   },
 });
