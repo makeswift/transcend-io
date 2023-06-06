0be71ca77724a830a64ee20c88cc46cb63bbd17e
Alan Pledger <alan@makeswift.com>
2023-06-06 19:45:41 -0400

    fix: sort cache key params

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,5 +4,9 @@ export const DEFAULT_FEED_PARAMS = {
 }
 
 export function getCacheKey(base: string, params: Record<string, any> = {}) {
-  return `${base}?${JSON.stringify(Object.entries(params).filter(([, v]) => v != null))}`
+  return `${base}?${JSON.stringify(
+    Object.entries(params)
+      .sort((a, b) => (a[0] < b[0] ? -1 : 1))
+      .filter(([, v]) => v != null),
+  )}`
 }
