29d7d0603c6a391f591eef505d61f4373e993d58
Alan Pledger <alan@makeswift.com>
2023-06-09 01:44:01 -0400

    chore: remove unused defaults

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,3 @@
-export const DEFAULT_FEED_PARAMS = {
-  limit: 30,
-  skip: 0,
-}
-
 export function getCacheKey(base: string, params: Record<string, any> = {}) {
   return `${base}?${JSON.stringify(
     Object.entries(params)
